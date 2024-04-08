d3f0c9b8bb3177b3a9e1321f3fac6e8ce44ef00b
Amine Badraoui <amine.badraoui@thescore.com>
2024-04-08 09:01:35 -0400

    wip

--- src/components/ui/sidebar.js
+++ src/components/ui/sidebar.js
@@ -3,15 +3,20 @@ import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { usePathname } from 'next/navigation';
 
+const Style = {
+  sidebar: "bg-primary py-8 w-1/5 border-0 shadow-md shadow-foreground",
+  buttonLink: "border-0 bg-transparent text-primary-foreground",
+}
+
 export const SideBar = ({ links, activeLink }) => {
   return (
-    <div className="bg-card py-8 w-1/5 border-0 shadow-md shadow-foreground ">
+    <div className={Style.sidebar}>
       {links.map((link, index) => {
         const isActive = link.href === usePathname();
 
         return (
           <div key={index} className="flex flex-col bg-transparent ">
-            <Button asChild variant={isActive ? 'active' : 'outline'} className="border-0 bg-transparent text-primary  ">
+            <Button asChild variant={isActive ? 'active' : 'outline'} className={Style.buttonLink}>
               {/* Update: Remove the <a> tag */}
               <Link className={link.href === usePathname()? "underline" : "no-underline"} href={link.href}>{link.name}</Link>
             </Button>
