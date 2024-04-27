9cd43f0eb34c512223a73356fa36866b96372643
Amine Badraoui <amine.badraoui@thescore.com>
2024-04-27 00:24:00 -0400

    fix new auth pages

--- src/pages/auth/success.js
+++ src/pages/auth/success.js
@@ -1,1 +1,54 @@
 // https://buy.stripe.com/eVaeWOaOW3uA7bG4gg Monthly link
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { Card } from '@/components/generic/card';
+import { TrendPulseHeader } from '@/components/layout/trendPulseHeader';
+
+const SuccessAuth = () => {
+  const [email, setEmail] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    // Trigger sending the magic link email
+    signIn('email', { email });
+  };
+
+  return (
+   
+
+    <div>
+       <TrendPulseHeader />
+
+       <div className='grid 
+       grid-cols-1 
+       h-dvh 
+       place-items-center'>
+       <Card className={`
+    w-96
+    h-96
+    `}>
+
+    <form className={
+      `flex flex-col`
+    } onSubmit={handleLogin}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+        required
+      />
+      <button type="submit">Send Magic Link</button>
+    </form>
+
+    </Card>
+
+       </div>
+
+   
+
+    </div>
+  );
+};
+
+export default SuccessAuth;
