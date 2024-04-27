f5fc929bcf5a696ae764ae0da9891a816d9eff61
Amine Badraoui <amine.badraoui@thescore.com>
2024-04-27 00:13:33 -0400

    add stripe webhook

---
+++ src/pages/api/stripe/webhook.js
@@ -0,0 +1,43 @@
+import { buffer } from 'micro';
+import Stripe from 'stripe';
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const webhookHandler = async (req, res) => {
+    if (req.method === 'POST') {
+        const buf = await buffer(req);
+        const sig = req.headers['stripe-signature'];
+
+        let event;
+
+        try {
+            event = stripe.webhooks.constructEvent(buf, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        } catch (err) {
+            console.error(`Webhook Error: ${err.message}`);
+            return res.status(400).send(`Webhook Error: ${err.message}`);
+        }
+
+        // Handle the event
+        switch (event.type) {
+            case 'payment_intent.succeeded':
+                const paymentIntent = event.data.object;
+                console.log(`PaymentIntent was successful!`);
+                break;
+            default:
+                console.log(`Unhandled event type ${event.type}`);
+        }
+
+        res.json({ received: true });
+    } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+    }
+};
+
+export default webhookHandler;
---
+++ src/pages/auth/success.js
@@ -0,0 +1,1 @@
+// https://buy.stripe.com/eVaeWOaOW3uA7bG4gg Monthly link
