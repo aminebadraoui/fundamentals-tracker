649b06b2351b5adb27e7f35f161b9df2077f5907
Amine Badraoui <amine.badraoui@thescore.com>
2024-05-05 13:14:24 -0400

    sidebar redesign

--- src/components/ui/sidebar.js
+++ src/components/ui/sidebar.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@/components/shadcn/accordion';
-import { useState } from 'react';
-
 
 const Style = {
   sidebarWrapper: "bg-primary hidden md:block py-4 w-[200px] border-0 shadow-md shadow-secondary min-h-screen",
-  linkStyle: "text-primary-foreground bg-primary cursor-pointer block w-full text-left px-4 py-4 font-medium text-sm", // Adjust font size, weight here
-  activeLink: "bg-secondary text-orange-500",
-  sectionHeader: "font-bold bg-primary text-sm border-b border-secondary-foreground text-secondary-foreground hover:bg-secondary cursor-pointer py-8 pl-4  w-full text-left",
+  linkStyle: "text-primary-foreground bg-primary cursor-pointer block w-full text-left px-4 py-4 font-medium text-sm",
+  activeLink: "text-orange-500 bg-secondary cursor-pointer block w-full text-left px-4 py-4 font-medium text-sm",
+  sectionHeader: "font-bold bg-primary text-sm border-b border-secondary-foreground text-secondary-foreground hover:bg-secondary cursor-pointer py-8 pl-4 w-full text-left",
   subLink: "pl-4 py-4"
 };
 
@@ -48,16 +46,26 @@ const links = [
   }
 ];
 
+
 export const SideBar = () => {
   const router = useRouter();
   const [openSection, setOpenSection] = useState(null);
 
-  const toggleSection = (index) => {
-    console.log(`Toggling section ${index}`);
-    setOpenSection(openSection === index ? null : index);
-  };
+  // This function checks if the current route is part of the section to decide if it should be open
+  useEffect(() => {
+    const path = router.asPath;
+    console.log(path)
+    const sectionIndex = links.findIndex(item => 
+      item.type === 'section' && item.links.some(subItem => path.includes(subItem.href))
+    );
+    if (sectionIndex !== -1) {
+      setOpenSection(sectionIndex);
+    }
+  }, [router.pathname]);
 
-  const isLinkActive = (href) => router.pathname === href;
+  const isLinkActive = (href) => {
+    return router.asPath === href;
+  };
 
   return (
     <div className={Style.sidebarWrapper}>
@@ -65,13 +73,13 @@ export const SideBar = () => {
         {links.map((item, index) => (
           item.type === 'section' ? (
             <AccordionItem key={index} value={index}>
-              <AccordionTrigger onClick={() => toggleSection(index)} className={Style.sectionHeader}>
+              <AccordionTrigger onClick={() => setOpenSection(index)} className={Style.sectionHeader}>
                 {item.name}
               </AccordionTrigger>
               <AccordionContent>
                 {item.links.map(subItem => (
                   <Link key={subItem.name} href={subItem.href} passHref>
-                    <div className={`${Style.linkStyle} ${isLinkActive(subItem.href) ? Style.activeLink : ''} `}>
+                    <div className={` ${ isLinkActive(subItem.href) ? Style.activeLink : Style.linkStyle }`}>
                       {subItem.name}
                     </div>
                   </Link>
@@ -79,8 +87,8 @@ export const SideBar = () => {
               </AccordionContent>
             </AccordionItem>
           ) : (
-            <Link key={item.name} href={item.href} passHref className=''>
-              <div className={`${Style.sectionHeader} ${isLinkActive(item.href) ? Style.activeLink : ''}`}>
+            <Link key={item.name} href={item.href} passHref>
+              <div className={`${ isLinkActive(item.href) ? Style.activeLink : Style.linkStyle }`}>
                 {item.name}
               </div>
             </Link>
