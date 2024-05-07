c78d4b59890067733c5146a432d15c4be1dd07dd
Amine Badraoui <amine.badraoui@thescore.com>
2024-05-07 14:49:56 -0400

    fix colors

--- src/utils/get-score-color.js
+++ src/utils/get-score-color.js
@@ -1,9 +1,9 @@
 const getScoreTextColor = (score) => {
-  return `font-bold ${score > 50 ? 'text-strongBuy' : score > 25 ? 'text-buy' : score > -25 ? 'text-neutral' : score > -50 ? 'text-sell'  : 'text-strongSell' }`
+  return `font-bold ${score > 50 ? 'text-strongBuy' : score >= 25 ? 'text-buy' : score >= -25 ? 'text-neutral' : score >= -50 ? 'text-sell'  : 'text-strongSell' }`
 }
 
 const getScoreBackgroundColor = (score) => {
-  return `font-bold ${score > 50 ? 'bg-strongBuy text-white ': score > 25 ? 'bg-buy text-white' : score > -25 ? 'bg-neutral text-black' : score > -50 ? 'bg-sell text-white'  : 'bg-strongSell text-white' }`
+  return `font-bold ${score > 50 ? 'bg-strongBuy text-white ': score >= 25 ? 'bg-buy text-white' : score >= -25 ? 'bg-neutral text-black' : score >= -50 ? 'bg-sell text-white'  : 'bg-strongSell text-white' }`
 }
 
 export { getScoreTextColor, getScoreBackgroundColor }
