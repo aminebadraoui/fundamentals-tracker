af8d2d8ccc4fbb880cfdae8754e4e2600d215a40
Amine Badraoui <amine.badraoui@thescore.com>
2024-05-07 08:32:59 -0400

    add error handling to event calendar

--- src/pages/api/event-calendar.js
+++ src/pages/api/event-calendar.js
@@ -1,33 +1,37 @@
 import { countryList_Iso3166 } from '@/utils/event-names';
 
 export default async (req, res) => {
-  const queryCountries = req.query.countries ? req.query.countries.split(',') : []
-  let countries = []
-  const limit = 600
+  const queryCountries = req.query.countries ? req.query.countries.split(',') : [];
+  let countries = [];
+  const limit = 600;
 
   if (queryCountries.length > 0) {
-    countries = queryCountries
-}
-  else {
-    countries = countryList_Iso3166
+    countries = queryCountries;
+  } else {
+    countries = countryList_Iso3166;
   }
- 
 
-  const promises = countries.map( async (country) => {
-      const eventsForCountry = await fetch(`https://eodhd.com/api/economic-events?api_token=${process.env.EOD_TOKEN}&fmt=json&country=${country}&limit=${limit}`, {cache: 'force-cache'});
-      const eventsForCountryData = await eventsForCountry.json();
-
-      return { [country]: eventsForCountryData }
+  const promises = countries.map(country => fetch(`https://eodhd.com/api/economic-events?api_token=${process.env.EOD_TOKEN}&fmt=json&country=${country}&limit=${limit}`, { cache: 'force-cache' })
+    .then(response => response.json())
+    .then(eventsForCountryData => ({ [country]: eventsForCountryData }))
+    .catch(error => {
+      console.error(`Error fetching events for ${country}:`, error);
+      return { [country]: { error: 'Failed to fetch data', details: error.message } }; // Return error details for each country
     })
+  );
 
-    Promise.all(promises).then((dataArray) => {  
-      const finalData = {}
-      for (const data in dataArray) {
-        Object.keys(dataArray[data]).map((key) => {
-          finalData[key] = dataArray[data][key]
-        })
-      }
- 
-      res.status(200).json(finalData)
+  Promise.all(promises)
+    .then(dataArray => {
+      const finalData = {};
+      dataArray.forEach(data => {
+        Object.keys(data).forEach(key => {
+          finalData[key] = data[key];
+        });
+      });
+      res.status(200).json(finalData);
     })
-}
+    .catch(error => {
+      console.error('Error processing promises:', error);
+      res.status(500).json({ error: 'Internal Server Error', details: 'Failed to process economic event data' });
+    });
+};
