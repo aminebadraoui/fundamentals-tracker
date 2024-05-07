291440369e0e74e80751182721d2c86d042ea539
Amine Badraoui <amine.badraoui@thescore.com>
2024-05-07 07:46:33 -0400

    fix auth

--- src/lib/withSession.js
+++ src/lib/withSession.js
@@ -3,16 +3,17 @@ import { getSession } from 'next-auth/react';
 // Higher-order function to wrap getServerSideProps for session checks
 const withSession = async (context, callback) => {
   const session = await getSession({ req: context.req });
+  const baseUrl = process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : 'http://localhost:3000'
 
   console.log("sessionFromWithSession", session)
-  // if (!session) {
-  //   return {
-  //     redirect: {
-  //       destination: '/auth/signin',
-  //       permanent: false,
-  //     },
-  //   };
-  // }
+  if (!session && baseUrl !== 'http://localhost:3000') {
+    return {
+      redirect: {
+        destination: '/auth/signin',
+        permanent: false,
+      },
+    };
+  }
   return callback(context, session);
 };
 
