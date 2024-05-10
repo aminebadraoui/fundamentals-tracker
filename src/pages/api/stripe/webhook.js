83746d70afbfe65d8e57f60187b66d5e6981869a
Amine Badraoui <amine.badraoui@thescore.com>
2024-05-10 19:34:10 -0400

    fix webhook file

--- src/pages/api/stripe/webhook.js
+++ src/pages/api/stripe/webhook.js
@@ -1,12 +1,10 @@
 import { buffer } from 'micro';
-import { on } from 'nodemailer/lib/xoauth2';
 import Stripe from 'stripe';
-
 import mongoose from 'mongoose';
 
 export const config = {
   api: {
-      bodyParser: false,
+    bodyParser: false,
   },
 };
 
@@ -14,119 +12,110 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 mongoose.connect(process.env.MONGODB_URI);
 
 const customerSchema = new mongoose.Schema({
-    email: String,
-    customerId: String,
-    subscription: {
-        paymentLink: String,
-        subscription: String,
-        status: String,
-    },
-  });
+  email: String,
+  customerId: String,
+  subscription: {
+    paymentLink: String,
+    subscription: String,
+    status: String,
+  },
+});
 
 const Customer = mongoose.models.Customer || mongoose.model('Customer', customerSchema);
 
 const onCheckoutSessionCompleted = async (session) => {
-    console.log(`Payment was successful!`);
-    console.log(session);
+  console.log(`Payment was successful!`);
+  console.log(session);
 
-    const customerId = session.customer
-    const customerEmail = session.customer_details.email;
-    const customerName = session.customer_details.name;
-    const paymentLink = session.payment_link
-    const paymentStatus = session.payment_status
-    const subscriptionId = session.subscription
+  const customerId = session.customer;
+  const customerEmail = session.customer_details.email;
+  const customerName = session.customer_details.name;
+  const paymentLink = session.payment_link;
+  const paymentStatus = session.payment_status;
+  const subscriptionId = session.subscription;
 
-    const subscription = {
-        paymentLink: paymentLink,
-        subscription: subscriptionId,
-        status: paymentStatus,
-    };
+  const subscription = {
+    paymentLink: paymentLink,
+    subscription: subscriptionId,
+    status: paymentStatus,
+  };
 
-    console.log(session.customer_details)
-    console.log(session.customer_details.email)
+  console.log(session.customer_details);
+  console.log(session.customer_details.email);
 
-    console.log(customerEmail)
-    
-    try {
-      let customer = await Customer.findOne({ email: customerEmail });
-      if (customer) {
-        // Customer exists, update their subscription
-    
-        customer.subscription = subscription;
-        await customer.save();
+  console.log(customerEmail);
 
-        console.log(`Subscription updated for ${email}`);
-        // TODO: send email to user
-      } else {
-        // Customer does not exist, create new one
-        const newCustomer = new Customer({
-          email: customerEmail,
-          customerId: customerId,
-          subscription: subscription,
-        });
+  try {
+    let customer = await Customer.findOne({ email: customerEmail });
+    if (customer) {
+      // Customer exists, update their subscription
+      customer.subscription = subscription;
+      await customer.save();
 
-        await newCustomer.save();
-        console.log(`New customer created with subscription for ${customerEmail}`);
+      console.log(`Subscription updated for ${customerEmail}`);
+      // TODO: send email to user
+    } else {
+      // Customer does not exist, create new one
+      const newCustomer = new Customer({
+        email: customerEmail,
+        customerId: customerId,
+        subscription: subscription,
+      });
 
-        // TODO: send email to user
-      }
-    } catch (error) {
-      console.error(`Error handling checkout.session.completed event: ${error}`);
-      // Handle the error appropriately
+      await newCustomer.save();
+      console.log(`New customer created with subscription for ${customerEmail}`);
+
+      // TODO: send email to user
     }
+  } catch (error) {
+    console.error(`Error handling checkout.session.completed event: ${error}`);
+    // Handle the error appropriately
+  }
 };
 
-
 const webhookHandler = async (req, res) => {
-    if (req.method === 'POST') {
-        const buf = await buffer(req);
-        const sig = req.headers['stripe-signature'];
+  if (req.method === 'POST') {
+    const buf = await buffer(req);
+    const sig = req.headers['stripe-signature'];
 
-        let event;
+    let event;
 
-        try {
-            event = stripe.webhooks.constructEvent(buf, sig, process.env.STRIPE_WEBHOOK_SECRET );
-           
-        } catch (err) {
-            console.error(`Webhook Error: ${err.message}`);
-            return res.status(400).send(`Webhook Error: ${err.message}`);
-        }
-
-        // Handle the event
-        switch (event.type) {
-          case 'checkout.session.completed':
-            const session = event.data.object;
-            // console.log(`checkout session completed !`);
-            // console.log(event)
-
-            onCheckoutSessionCompleted(session);
-            break;
-
-            case 'setup_intent.succeeded':
-                const paymentIntent = event.data.object;
-                // console.log(`PaymentIntent was successful!`);
-                // console.log(event)
-                break;
-
-            case 'customer.updated':
-            //   console.log(`customer updated`);
-            //   console.log(event)
-              break;
-
-            case 'invoice.finalized':
-            //   console.log(`invoice finalized`);
-            //   console.log(event)
-              break;
-
-            default:
-                console.log(`Unhandled event type ${event.type}`);
-        }
-
-        res.json({ received: true });
-    } else {
-        res.setHeader('Allow', 'POST');
-        res.status(405).end('Method Not Allowed');
+    try {
+      event = stripe.webhooks.constructEvent(buf, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+      console.error(`Webhook Error: ${err.message}`);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
     }
+
+    // Handle the event
+    switch (event.type) {
+      case 'checkout.session.completed':
+        const session = event.data.object;
+        await onCheckoutSessionCompleted(session);
+        break;
+
+      case 'setup_intent.succeeded':
+        const paymentIntent = event.data.object;
+        // Handle setup intent success
+        break;
+
+      case 'customer.updated':
+        // Handle customer update
+        break;
+
+      case 'invoice.finalized':
+        // Handle invoice finalized
+        break;
+
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    res.json({ received: true });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
 };
 
 export default webhookHandler;
